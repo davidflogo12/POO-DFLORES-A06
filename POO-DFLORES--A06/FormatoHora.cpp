cf059894f1f707eb70fff473413cdbe4ce3c0d44
davidflogo12 <35820988+davidflogo12@users.noreply.github.com>
2018-03-02 18:57:59 -0600

    Update FormatoHora.cpp

--- POO-DFLORES--A06/FormatoHora.cpp
+++ POO-DFLORES--A06/FormatoHora.cpp
@@ -3,48 +3,111 @@
 //
 
 #include "FormatoHora.h"
-#include <cmath>
-FormatoHora::FormatoHora(int h, int m, int s) {
-    this->horas=h;
-    this->minutos=m;
-    this->segundos=s;
-    std::cout<<this->to_String()<<"\n";
+#include <iostream>
+
+FormatoHora::FormatoHora( int h, int m, int s){
+    this -> horas=h;
+    this -> minutos=m;
+    this ->segundos=s;
+
 }
 
-FormatoHora operator+ (const FormatoHora &hi, const FormatoHora &hf){
-    int resA = (hf.horas + hi.segundos);
-    int resB = (hf.minutos+ hi.minutos );
-    int resC = (hf.segundos+ hi.segundos);
+FormatoHora operator+ (const FormatoHora &hi, const FormatoHora &hf ){
+    int hr, mr, sr;
 
-    if(resC >= 60){
-        resB= resB++;
-    }
-    else if(resB >= 60){resA =
-        resA++;
+    hr = hi.horas + hf.horas;
+    mr = hi.minutos + hf.minutos;
+    sr = hi.segundos + hf.segundos;
 
-    }
-    FormatoHora resultado = *new FormatoHora(resC, resB, resA);
-    return resultado;
+
+    FormatoHora Hresult = *new FormatoHora(hr, mr, sr);
+    Hresult.validar();
+    return Hresult;
 }
-FormatoHora operator- (const FormatoHora &hi, const FormatoHora &hf){
-    int resA = (hf.horas + hi.segundos);
-    int resB = (hf.minutos+ hi.minutos );
-    int resC = (hf.segundos+ hi.segundos);
+std::string FormatoHora::to_String() {
+    std::string Thoras, Tsegundos, Tminutos;
 
-    if(resC >= 60){
-        resB= resB++;
-    }
-    else if(resB >= 60){resA =
-                                resA++;
+    Thoras = std::to_string(gethora());
+    Tsegundos = std::to_string(getsegundos());
+    Tminutos = std::to_string(getminutos());
+
+    if (getminutos() < 10 && getsegundos() <10) {
+        return Thoras+ ":0"+Tminutos+":0"+Tsegundos;
 
     }
-    FormatoHora resultado = *new FormatoHora(resC, resB, resA);
-    return resultado;
+    else if (getminutos()< 10) {
+        return Thoras+ ":0"+Tminutos+":"+Tsegundos;
+    }
+    else if (getsegundos() < 10) {
+        return Thoras+ ":"+Tminutos+":0"+Tsegundos;
+    }
+    else {
+        return Thoras+ ":"+Tminutos+":"+Tsegundos;
+    }
+
+
+
 }
 
 
 
 
+
+FormatoHora operator- (const FormatoHora &hi, const FormatoHora &hf ) {
+    int hr, mr, sr;
+
+    hr = hi.horas - hf.horas;
+    mr = hi.minutos - hf.minutos;
+    sr = hi.segundos - hf.segundos;
+
+    FormatoHora Hresult = *new FormatoHora(hr, mr, sr);
+    Hresult.validar();
+    return Hresult;
+
+}
+
+
+bool FormatoHora::validar()  {
+    int hr,mr,sr;
+
+    hr= gethora();
+    mr= getminutos();
+    sr= getsegundos();
+
+    if (sr > 60){
+        sr = sr -60;
+        mr++;
+    }
+    if (mr > 60){
+        mr = mr - 60;
+        hr++;
+    }
+    if (hr > 24){
+        hr = hr - 24;
+    }
+
+
+}
+
+
+int FormatoHora::gethora(){
+    return horas;
+}
+int FormatoHora::getminutos(){
+    return minutos;
+}
+int FormatoHora::getsegundos(){
+    return this->segundos;}
+
+void FormatoHora::sethora(const int h){
+    FormatoHora::horas=h; }
+
+void FormatoHora::setmintuos(const int m){
+    FormatoHora::minutos = m ;
+}
+void FormatoHora::setsegundos(const int s){
+    FormatoHora::segundos = s;
+}
 
 
 
